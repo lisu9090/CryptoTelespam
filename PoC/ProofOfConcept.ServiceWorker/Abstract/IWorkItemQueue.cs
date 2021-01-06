f6f2b5f219b018a38b76ea04248666565bdb02a6
lisu9090 <lisu9090@gmail.com>
2021-01-06 16:46:46 +0100

    poc queue

---
+++ PoC/ProofOfConcept.ServiceWorker/Abstract/IWorkItemQueue.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProofOfConcept.ServiceWorker.Abstract
+{
+    interface IWorkItemQueue<T> // where T : base for work items
+    {
+        void Push(T workItem);
+        T Pop();
+    }
+}
