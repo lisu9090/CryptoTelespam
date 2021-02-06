dbab4c7c266035ce0be60602c7cef438720ba2d0
lisu9090 <lisu9090@gmail.com>
2021-02-06 08:26:13 +0100

    nupl mechanism

--- PoC/ProofOfConcept.Domain/Domain/DataProcess/NuplEventDetectorService.cs
+++ PoC/ProofOfConcept.Domain/Domain/DataProcess/NuplEventDetectorService.cs
@@ -1,18 +1,32 @@
-﻿using ProofOfConcept.Abstract.Domain.Model;
-using ProofOfConcept.Abstract.Domain;
+﻿using ProofOfConcept.Abstract.Domain;
+using ProofOfConcept.Abstract.Domain.Model;
+using ProofOfConcept.Domain.Const.Code;
 using System.Threading.Tasks;
-using ProofOfConcept.Domain.Abstract;
-using ProofOfConcept.Domain.Domain.NuplLevelChange;
 
 namespace ProofOfConcept.Domain.Domain.DataProcess
 {
     public class NuplEventDetectorService : IDataProcessorService<Nupl>
     {
+        private const float CAPITULATION_LEVEL = 0;
+        private const float BELIEFE_LEVEL = 0.25f;
+        private const float OPTIMISM_LEVEL = 0.5f;
+        private const float HOPE_LEVEL = 0.75f;
+
+
         public Task<StockEvent<Nupl>> DetectEventAsync(Nupl data)
         {
             return Task.Run(() => 
             {
-                //INuplLevelChangeDetector decoratedDetector = new CapitulationLevelDetector();
+                var eventCode = "";
+               
+                if(data.PreviousValue > data.Value)
+                {
+
+                }
+                else if (data.Value > data.PreviousValue)
+                {
+
+                }
 
                 return (StockEvent<Nupl>)null;
             });
