77ebd4a353cf93ad541b6b4cc475c0193d2272f4
lisu9090 <lisu9090@gmail.com>
2021-03-14 21:08:37 +0100

    pase issue fix

--- PoC/ProofOfConcept.ApiClient.DI/DependencyInjection.cs
+++ PoC/ProofOfConcept.ApiClient.DI/DependencyInjection.cs
@@ -9,20 +9,38 @@ namespace ProofOfConcept.ApiClientDomain
 {
     public static class DependencyInjection
     {
+        private const int DEFAULT_TIMEOUT = 30;
+
         public static void RegisterApiClients(this IServiceCollection services, IConfiguration configuration)
         {
+            int timeout = DEFAULT_TIMEOUT;
+
             services.AddHttpClient();
 
+            if (configuration.GetSection("Api:GlassNode:Timeout").Exists())
+            {
+                int.TryParse(configuration["Api:GlassNode:Timeout"], out timeout);
+            }
+
             services.AddTransient<IRestApiService, GlassNodeApiService>(sp => 
-                new GlassNodeApiService(int.Parse(configuration["Api:GlassNode:Timeout"]),
+                new GlassNodeApiService(timeout,
                     configuration["Api:GlassNode:BaseUrl"],
                     configuration["Api:GlassNode:ApiKeyParamName"],
                     configuration["Api:GlassNode:Key"],
                     sp.GetRequiredService<HttpClient>(),
                     sp.GetRequiredService<ILogger<GlassNodeApiService>>()));
 
+            if (configuration.GetSection("Api:Telegram:Timeout").Exists())
+            {
+                int.TryParse(configuration["Api:Telegram:Timeout"], out timeout);
+            }
+            else
+            {
+                timeout = DEFAULT_TIMEOUT;
+            }
+
             services.AddTransient<IMessageApiService, TelegramMessageApiService>(sp =>
-                new TelegramMessageApiService(int.Parse(configuration["Api:Telegram:Timeout"]),
+                new TelegramMessageApiService(timeout,
                     configuration["Api:Telegram:BaseUrl"],
                     configuration["Api:Telegram:MessageTargetParamName"],
                     configuration["Api:Telegram:MessageTarget"],
