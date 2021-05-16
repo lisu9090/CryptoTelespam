fb3c162926741fa2a66043a4f6ea40ac088fa1c2
szopson <jakub.baranowski1@googlemail.com>
2021-05-16 10:34:24 +0100

    MvrvZScore indicator added with zones

---
+++ PoC/ProofOfConcept.Domain/Zone/MvrvZScoreZone/MvrvZScoreEuphoriaZone.cs
@@ -0,0 +1,21 @@
+﻿using ProofOfConcept.Domain.Indicator;
+using ProofOfConcept.Domain.Indicator.Abstract;
+using ProofOfConcept.Domain.Zone.Abstract;
+using System;
+using System.Linq;
+using System.Collections.Generic;
+using ProofOfConcept.Common;
+
+namespace ProofOfConcept.Domain.Zone.MvrvZScoreZone
+{
+    public class MvrvZScoreEuphoriaZone : IndicatorZone<MvrvZScore>
+    {
+        internal class ZoneRange : IZoneRange<MvrvZScoreEuphoriaZone, MvrvZScore, float>
+        {
+            private Range _range = Range.And(x => x >= 7.0f, x => x <= double.MaxValue);
+            public bool IsInZone(float value) => _range.IsInRange(value);
+        }
+
+        public override string Name => "Euphoria";
+    }
+}
---
+++ PoC/ProofOfConcept.Domain/Zone/MvrvZScoreZone/MvrvZScoreZoneService.cs
@@ -0,0 +1,34 @@
+﻿using ProofOfConcept.Domain.Indicator;
+using ProofOfConcept.Domain.Zone.Abstract;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ProofOfConcept.Domain.Zone.MvrvZScoreZone
+{
+    public class MvrvZScoreZoneService
+    {
+        private readonly IEnumerable<IZoneRange<IndicatorZone<MvrvZScore>, MvrvZScore, float>> _zoneRanges;
+
+        public MvrvZScoreZoneService()
+        {
+            _zoneRanges = new List<IZoneRange<IndicatorZone<MvrvZScore>, MvrvZScore, float>>
+            {
+                new MvrvZScoreCapitulationZone.ZoneRange(),
+                new MvrvZScoreEuphoriaZone.ZoneRange(),
+                new MvrvZScoreHopeZone.ZoneRange(),
+                new MvrvZScoreOptimismZone.ZoneRange(),
+                new MvrvZScoreBeliefZone.ZoneRange()
+            };
+        }
+
+        public IndicatorZone<MvrvZScore> GetZone(float value)
+        {
+            IZoneRange<IndicatorZone<MvrvZScore>, MvrvZScore, float> zoneRange = _zoneRanges
+                .Where(range => range.IsInZone(value))
+                .FirstOrDefault();
+
+            return new IndicatorZoneActivator()
+                .CreateZoneInstance(zoneRange);
+        }
+    }
+}
